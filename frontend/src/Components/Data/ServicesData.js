e08ab58f137ee7c052b80e86470239a56af8dd42
Sai Kiran Bheemagani <142832467+saikiranbheemagani@users.noreply.github.com>
2025-06-05 14:05:08 -0400

    General data arrays for FAQs, services, etc.
    General data arrays for FAQs, services, etc.

---
+++ frontend/src/Components/Data/ServicesData.js
@@ -0,0 +1,9 @@
+// projectsdata.js
+export const ServicesData = [
+    { id: 1, title: 'Project 1', description: 'Description for Project 1' },
+    { id: 2, title: 'Project 2', description: 'Description for Project 2' },
+    // Add more project data here
+  ];
+  
+
+  
