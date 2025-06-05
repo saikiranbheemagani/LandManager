dd47b266badb7a1a80934efab755dc83a90129ca
Sai Kiran Bheemagani <142832467+saikiranbheemagani@users.noreply.github.com>
2025-06-05 14:12:04 -0400

    Create Additional.js

---
+++ frontend/src/Data/Additional.js
@@ -0,0 +1,45 @@
+import { Search, Land,Broom, Documents,Auctionc,Spinach, Vest,Bolt } from "../assets"
+
+
+export default [
+    {
+        id : "1",
+        name : "Plot tracing( find)",
+        image : Search,
+    },
+    {
+        id : "2",
+        name : "Plot Demarcation( with Survey)",
+        image : Land,
+    },
+    {
+        id : "3",
+        name : "Documents and Paperwork",
+        image : Documents,
+    },
+    {
+        id : "4",
+        name : "Cleaning and Fencing",
+        image : Broom,
+    },
+    {
+        id : "5",
+        name : "Legal services if needed",
+        image : Auctionc,
+    },
+    {
+        id : "6",
+        name : "Planting saplings & beautification of plot",
+        image : Spinach,
+    },
+    {
+        id : "7",
+        name : "All Construction work services",
+        image : Vest,
+    },
+    {
+        id : "8",
+        name : "Water, Electricity and Sanitation ( Permissions & Connections) ",
+        image : Bolt,
+    },
+];
