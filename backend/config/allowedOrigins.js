47a0c252b6a35786200fd5f7ca57f303591adab8
Sai Kiran Bheemagani <142832467+saikiranbheemagani@users.noreply.github.com>
2025-06-05 14:43:10 -0400

    Create allowedOrigins.js

---
+++ backend/config/allowedOrigins.js
@@ -0,0 +1,11 @@
+const allowedOrigins = [
+    'https://www.landmanager.com',
+    'http://127.0.0.1:5500',
+    'http://localhost:5000',
+    'http://localhost:3500',
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'https://landmanager-app.netlify.app'
+];
+
+module.exports = allowedOrigins;
